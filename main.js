ef205d7cc5e4567c58473c6f7e57bcd4a0c4876d
WiseElfie2009 <83706514+WiseElfie2009@users.noreply.github.com>
2021-05-25 18:10:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var veg=["Margherita","Double Cheese Margherita","Farm House","Peppy Paneer","Mexican Green Wave","Deluxe Veggie","Veg Extravaganza","CHEESE N CORN","PANEER MAKHANI","VEGGIE PARADISE","FRESH VEGGIE","Indi Tandoori Paneer"];
+veg.sort;
+document.getElementById("vegan").innerHTML=veg;
+var non_veg =["PEPPER BARBECUE CHICKEN","CHICKEN SAUSAGE","Chicken Golden Delight","Non Veg Supreme","Chicken Dominator","PEPPER BARBECUE & ONION","CHICKEN FIESTA","Indi Chicken Tikka"];
+non_veg.sort;
+document.getElementById("non_vegan").innerHTML=veg;
